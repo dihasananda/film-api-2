a30578aefb488b1d2c195c1b72cbe5cb2e9b1c0a
Dihas Ananda <101080895+dihasananda@users.noreply.github.com>
2023-10-09 14:25:45 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FilmsModule } from './films/films.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), FilmsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), FilmsModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
